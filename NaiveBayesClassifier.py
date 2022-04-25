bdfc6d349d965cc8732a9d9a21dc9f289244283a
AG <101548034+IAINTREADYFORGRAD@users.noreply.github.com>
2022-04-25 15:22:55 +0800

    Add files via upload

---
+++ NaiveBayesClassifier.py
@@ -0,0 +1,72 @@
+from nltk.corpus.reader import PlaintextCorpusReader
+from nltk.probability import FreqDist
+import random
+
+n = 300
+
+Gossiping_dir = "Gossiping/May 27/"
+pcr1 = PlaintextCorpusReader(root=Gossiping_dir, fileids=".*\.txt") 
+#.*\ 是所有檔案的意思
+
+c = "Gossiping"
+Gossiping_documents = [(pcr1.words(fileid),c) for fileid in pcr1.fileids()]
+#print(Gossiping_documents[:9])
+
+C_Chat_dir = "C_Chat/Sat May 15/"
+pcr2 = PlaintextCorpusReader(root=C_Chat_dir, fileids=".*\.txt")
+
+c = "C_Chat"
+C_Chat_documents = [(pcr2.words(fileid),c) for fileid in pcr2.fileids()]
+#(pcr2.words(fileid),c) 是 tuple
+#word definition 裡的self是甚麼意思??
+#print(C_Chat_documents[:9]) #印出 sat may那一個資料夾的，因為 C_Chat_dir = "C_Chat/Sat May 15/"
+
+
+documents = Gossiping_documents + C_Chat_documents 
+#documents就變成一個list裡面裝超多g/c list
+#print(documents[0])
+#print(documents[-1]) #印出documents的最後一個元素
+
+random.shuffle(x=documents) 
+# Different results each time? same; otherwise, the random.seed() is required
+
+import datetime
+#print(datetime.datetime.now())
+
+N_features = 2000
+all_words = FreqDist(pcr1.words() + pcr2.words())   # 20 seconds...??
+#print(all_words)#<FreqDist with 33003 samples and 530403 outcomes>
+#all_words只是物件，只會印出資訊
+word_features = list(all_words)[:N_features] 
+#出現頻率最高的0-1999(N_features)個字
+#word_features會把all_words變成list，就可以印出一堆東西
+#print(word_features) #[':', '/', '.', '05', '推', '→', '24', '23', '，', '的', '了', '是', '有', '-', '？', '噓....
+
+def document_features(document_words):
+    document_words = set(document_words)
+    #在資料結構上有排序，找東西比較快。所以是可以用list的。
+    features = {}
+    for word in word_features:
+        features['contains({})'.format(word)] = (word in document_words) #是簡式嗎，原來長甚麼樣子
+    return features
+#print(document_features(documents[0][0]))
+#documents怎麼會是二維的，是tuple，所以這裡只是查看，沒有甚麼意思
+#'contains(跟)': True
+
+N_testing = 100
+#print(datetime.datetime.now())
+featuresets = [(document_features(d), c) for (d,c) in documents]    # 15 seconds...
+train_set, test_set = featuresets[N_testing:], featuresets[:N_testing]
+
+from nltk import NaiveBayesClassifier
+classifier = NaiveBayesClassifier.train(train_set)  
+#print(datetime.datetime.now()) 
+from nltk import classify
+print(classify.accuracy(classifier, test_set)) #1.0
+
+print(classifier.show_most_informative_features(5))
+
+# What are the most informative training features in your PTT text prediction task? 
+# do they make sense to you?
+
+
---
+++ w2v.py
@@ -0,0 +1,44 @@
+from nltk.corpus.reader import PlaintextCorpusReader
+from nltk.probability import FreqDist
+
+import urllib
+file = urllib.request.urlopen(url="https://raw.githubusercontent.com/stopwords-iso/stopwords-zh/master/stopwords-zh.txt")
+stopwords = file.read().decode("utf8").split()
+#print(stopwords)
+
+from string import printable
+n = 300
+
+Gossiping_dir = "Gossiping/May 27/"
+pcr = PlaintextCorpusReader(root=Gossiping_dir, fileids=".*\.txt") #.*\. ??
+fd = FreqDist(samples=pcr.words())
+Gossiping_words = [word for word,freq in fd.most_common(n=n) if word not in stopwords and word[0] not in printable]
+
+source_dir = "C_Chat/Sat May 15/"
+pcr = PlaintextCorpusReader(root=source_dir, fileids=".*\.txt")
+fd = FreqDist(samples=pcr.words())
+C_Chat_words = [word for word,freq in fd.most_common(n=n) if word not in stopwords and word[0] not in printable]
+
+print(Gossiping_dir)
+print([word for word in Gossiping_words if word not in C_Chat_words])
+
+print(source_dir)
+print([word for word in C_Chat_words if word not in Gossiping_words])
+
+
+# 1. What are the most common words in your two PTT boards respectively? 
+#    Do they correspond to what you have expected?
+#    (Please upload your Python script to GitHub, and paste your GitHub link on the online text of eCourse homework.)
+
+
+from gensim.models import Word2Vec
+from gensim.models.word2vec import PathLineSentences
+corpus = PathLineSentences(Gossiping_dir)#"Gossiping")
+
+model = Word2Vec(sentences=corpus, vector_size=100, window=5, min_count=1, workers=4)
+print(model.wv.most_similar(positive=['女性',"老公"], negative=['男性']))
+
+# 2. By using the GenSim Word2Vec module, 
+#    find a word x in an analogy like "man : king :: woman : x" (read: man is to king as woman is to x) 
+#    in your PTT texts.
+#    (Please upload your Python script to GitHub, and paste your GitHub link on the online text of eCourse homework.)
